a2c23518baaa63118d6918f046e2c5fcf5039a75
birkin <birkin.diana@gmail.com>
2016-04-29 09:01:44 -0400

    adds modelform.

---
+++ email_app/formstuff.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from .models import Invitation
+from django import forms
+from django.utils.crypto import get_random_string
+
+
+class InvitationForm( forms.ModelForm ):
+
+    class Meta:
+        model = Invitation
+        fields = ['email']
+
+    def save(self, *args, **kwargs):
+        self.instance.key = get_random_string(32).lower()
+        return super(InvitationForm, self).save(*args, **kwargs)
