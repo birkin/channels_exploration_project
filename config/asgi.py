a1ce44af881f39c0e64371eb9820ab2e7825d922
birkin <birkin.diana@gmail.com>
2016-05-23 09:04:14 -0400

    tweaks formatting.

--- config/asgi.py
+++ config/asgi.py
@@ -1,5 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
 import os
 import channels.asgi
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chnl_expl_project.config.settings")
 channel_layer = channels.asgi.get_channel_layer()
