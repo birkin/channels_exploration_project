e862122c42d2e085e43eb6fd81b61065d88d7f7b
birkin <birkin.diana@gmail.com>
2016-10-31 15:55:53 -0400

    got email to get to send point.

--- email_app/consumers.py
+++ email_app/consumers.py
@@ -7,7 +7,7 @@ For django channels controller (akin to views.py).
 Activated by config/routing.py
 """
 
-import logging
+import logging, pprint, time
 # from django.contrib.sites.models import Site
 from django.core.mail import EmailMessage
 from django.utils import timezone
@@ -19,19 +19,24 @@ log = logging.getLogger(__name__)
 log.debug( 'consumers.py loaded' )
 
 
-def send_invite(message):
+def send_invite( message_obj ):
     log.debug( 'starting send_invite()' )
+    time.sleep( 1 )
     try:
-        invite = Invitation.objects.get(
-            id=message.content.get('id'),
-        )
-    except Invitation.DoesNotExist:
+        log.debug( 'message_obj, `{}`'.format(message_obj) )
+        log.debug( 'message_obj.__dict__, `{}`'.format(pprint.pformat(message_obj.__dict__)) )
+        notification_dct = message_obj.content
+        log.debug( 'notification_dct, `{}`'.format(notification_dct) )
+        invite_key = notification_dct['key']
+        log.debug( 'invite_key, `{}`'.format(invite_key) )
+        invite = Invitation.objects.get( key=invite_key )
+    except Exception as e:
+        log.error( 'e, ```{}```'.format(unicode(repr(e))) )
         log.error("Invitation to send not found")
         return
-
     subject = "You've been invited!"
     body = "Go to https://%s/invites/accept/%s/ to join!" % (
-            Site.objects.get_current().domain,
+            'foo',
             invite.key,
         )
     try:
@@ -46,3 +51,31 @@ def send_invite(message):
         invite.save()
     except:
         log.exception('Problem sending invite %s' % (invite.id))
+
+# def send_invite(message):
+#     log.debug( 'starting send_invite()' )
+#     try:
+#         invite = Invitation.objects.get(
+#             id=message.content.get('id'),
+#         )
+#     except Invitation.DoesNotExist:
+#         log.error("Invitation to send not found")
+#         return
+
+#     subject = "You've been invited!"
+#     body = "Go to https://%s/invites/accept/%s/ to join!" % (
+#             Site.objects.get_current().domain,
+#             invite.key,
+#         )
+#     try:
+#         message = EmailMessage(
+#             subject=subject,
+#             body=body,
+#             from_email="from_email",
+#             to=[invite.email,],
+#         )
+#         message.send()
+#         invite.sent = timezone.now()
+#         invite.save()
+#     except:
+#         log.exception('Problem sending invite %s' % (invite.id))
--- email_app/formstuff.py
+++ email_app/formstuff.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-import logging
+import logging, pprint
 from .models import Invitation
 from channels import Channel
 from django import forms
@@ -20,7 +20,12 @@ class InvitationForm( forms.ModelForm ):
 
     def save(self, *args, **kwargs):
         self.instance.key = get_random_string( 32, allowed_chars='abcdefghjkmnpqrstuvwxyz23456789' )
-        return super(InvitationForm, self).save(*args, **kwargs)
+        invitation_instance = super(InvitationForm, self).save(*args, **kwargs)
+        return invitation_instance
+
+    # def save(self, *args, **kwargs):
+    #     self.instance.key = get_random_string( 32, allowed_chars='abcdefghjkmnpqrstuvwxyz23456789' )
+    #     return super(InvitationForm, self).save(*args, **kwargs)
 
     # end class InvitationForm
 
@@ -35,11 +40,23 @@ class ChannelsInvitationForm( forms.ModelForm ):
     def save(self, *args, **kwargs):
         log.debug( 'in ChannelsInvitationForm.save()' )
         self.instance.key = get_random_string( 32, allowed_chars='abcdefghjkmnpqrstuvwxyz23456789' )
-        response = super(ChannelsInvitationForm, self).save(*args, **kwargs)
-        notification = {
-            'id': self.instance.id,
-        }
+        channel_invitation_instance = super(ChannelsInvitationForm, self).save(*args, **kwargs)
+        log.debug( 'type(channel_invitation_instance), `{}`'.format(type(channel_invitation_instance)) )
+        log.debug( 'channel_invitation_instance.__dict__, `{}`'.format(pprint.pformat(channel_invitation_instance.__dict__)) )
+        notification_dct = { 'key': channel_invitation_instance.key }
+        log.debug( 'notification_dct, `{}`'.format(notification_dct) )
         log.debug( 'about to hit `Channel.send()`, which should trigger config.routing to perceive the `send-invite` string and have email_app.consumers.send_invite() do work.' )
-        Channel('send-invite').send( notification )
-        log.debug( 'in ChannelsInvitationForm.save(); about to return response of type, `{}`'.format(type(response)) )
-        return response
+        Channel('send-invite').send( notification_dct )
+        return channel_invitation_instance
+
+    # def save(self, *args, **kwargs):
+    #     log.debug( 'in ChannelsInvitationForm.save()' )
+    #     self.instance.key = get_random_string( 32, allowed_chars='abcdefghjkmnpqrstuvwxyz23456789' )
+    #     response = super(ChannelsInvitationForm, self).save(*args, **kwargs)
+    #     log.debug( 'type(response), `{}`'.format(type(response)) )
+    #     notification_dct = { 'id': self.instance.id }
+    #     log.debug( 'notification_dct, `{}`'.format(notification_dct) )
+    #     log.debug( 'about to hit `Channel.send()`, which should trigger config.routing to perceive the `send-invite` string and have email_app.consumers.send_invite() do work.' )
+    #     Channel('send-invite').send( notification_dct )
+    #     log.debug( 'in ChannelsInvitationForm.save(); about to return response of type, `{}`'.format(type(response)) )
+    #     return response
